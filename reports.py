65d32ed9def352381f965210465cea35b8b9f7a7
James Chapman <chapmanjamesg@gmail.com>
2020-02-06 08:33:44 -0600

    finished exercise part 4

--- reports.py
+++ reports.py
@@ -1,6 +1,8 @@
 import sqlite3
 from student import Student
 from cohort import Cohort
+from exercise import Exercise
+from instructor import Instructor
 
 class StudentExerciseReports():
 
@@ -59,6 +61,58 @@ class StudentExerciseReports():
             for cohort in all_cohorts:
                 print(cohort.Name)
 
+    def all_exercises(self):
+
+        """Retrieve all exercises"""
+
+        with sqlite3.connect(self.db_path) as conn:
+            conn.row_factory = lambda cursor, row:Exercise(
+                row[0], row[1]
+            )
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            select
+                e.Name,
+                e.Language
+            from exercise e 
+            """)
+
+            all_exercises = db_cursor.fetchall()
+
+            for exercise in all_exercises:
+                print(f'{exercise.Name} using {exercise.Language}')
+
+    def all_instructors(self):
+
+        """Retrieve all instructors with the cohort name"""
+
+        with sqlite3.connect(self.db_path) as conn:
+            conn.row_factory = lambda cursor, row:Instructor(
+                row[1], row[2], row[3], row[4], row[5]
+            )
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            select i.Instructor_Id,
+                i.First_Name,
+                i.Last_Name,
+                i.Slack_Handle,
+                i.Cohort_Id,
+                c.name
+            from Instructor i
+            join Cohort c on i.Cohort_Id = c.Cohort_Id
+            order by i.Cohort_Id
+            """)
+
+            all_instructors = db_cursor.fetchall()
+
+            for instructor in all_instructors:
+                print(f'{instructor.First_Name} {instructor.Last_Name} is in {instructor.Cohort}')
+
+
 reports = StudentExerciseReports()
 # reports.all_students()
-reports.all_cohorts()
+# reports.all_cohorts()
+reports.all_exercises()
+# reports.all_instructors()
